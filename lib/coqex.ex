25df1958ce10b46f5dae5b38aab15a93a75100f5
Susumu Yamazaki <zacky1972@gmail.com>
2025-01-05 10:33:35 +0900

    feature: implement Coqex.docker_executable() and extract it from Coqex.docker_exists?()

--- lib/coqex.ex
+++ lib/coqex.ex
@@ -3,12 +3,20 @@ defmodule Coqex do
   Coqex: A Coq-Elixir binding.
   """
 
+  @spec docker_executable() :: binary() | nil
+  @doc """
+  Returns Locates an executable of `docker` on the system.
+  """
+  def docker_executable() do
+    System.find_executable("docker")
+  end
+
   @spec docker_exists?() :: boolean()
   @doc """
   Returns `true` if `docker` is available.
   """
   def docker_exists?() do
-    System.find_executable("docker")
+    docker_executable()
     |> is_nil()
     |> Kernel.!()
   end
