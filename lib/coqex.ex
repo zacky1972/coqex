4178b18fd5df7fdab80b07318d3f0d362a1eebf7
Susumu Yamazaki <zacky1972@gmail.com>
2025-01-05 10:25:08 +0900

    feature: implement Coqex.docker_exists?

--- lib/coqex.ex
+++ lib/coqex.ex
@@ -2,4 +2,14 @@ defmodule Coqex do
   @moduledoc """
   Coqex: A Coq-Elixir binding.
   """
+
+  @spec docker_exists?() :: boolean()
+  @doc """
+  Returns `true` if `docker` is available.
+  """
+  def docker_exists?() do
+    System.find_executable("docker")
+    |> is_nil()
+    |> Kernel.!()
+  end
 end
