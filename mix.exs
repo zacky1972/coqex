ea5b8607a64fcd092f64f970332dfb431d9d0730
Susumu Yamazaki <zacky1972@gmail.com>
2025-01-05 10:06:57 +0900

    Install and set up dialyxir and dialyzer (#6)
    * build: install and set up dialyxir
    
    * ci: add dialyzer to workflow

--- mix.exs
+++ mix.exs
@@ -9,6 +9,12 @@ defmodule Coqex.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
+      # Dialyzer
+      dialyzer: [
+        plt_local_path: "priv/plts/project.plt",
+        plt_core_path: "priv/plts/core.plt"
+      ],
+
       # Docs
       name: "Coqex",
       source_url: "https://github.com/zacky1972/coqex",
@@ -29,6 +35,7 @@ defmodule Coqex.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.34", only: :dev, runtime: false}
     ]
   end
