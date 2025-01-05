bf09d6e26a9b608a00094d1e86f7484a77314588
Susumu Yamazaki <zacky1972@gmail.com>
2025-01-05 10:17:27 +0900

    remove hello

--- lib/coqex.ex
+++ lib/coqex.ex
@@ -2,17 +2,4 @@ defmodule Coqex do
   @moduledoc """
   Coqex: A Coq-Elixir binding.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Coqex.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/coqex_test.exs
+++ test/coqex_test.exs
@@ -1,8 +1,4 @@
 defmodule CoqexTest do
   use ExUnit.Case
   doctest Coqex
-
-  test "greets the world" do
-    assert Coqex.hello() == :world
-  end
 end
